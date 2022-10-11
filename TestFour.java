0ed4ddbd9e5c3a344a9d73db55cf783fac693dad
balakrishnan <balakrish23579@gmail.com>
2022-10-11 22:24:08 +0530

    java Test cases

---
+++ TestFour.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TestFour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        Scanner scan = new Scanner(System.in);
+		        int a = scan.nextInt();
+		        for(int i =0 ;i<=a;i++){
+		          System.out.println(a);
+		        }
+		        // Complete this line
+		        // Complete this line
+		        a++;
+		    }
+
+}
